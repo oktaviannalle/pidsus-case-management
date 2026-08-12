2b147f823642b7e4442448f12c4ae4c8df8044e5
oktaviannalle <oktavian.putra@formulatrix.com>
2026-08-12 18:37:09 +0700

    fix(app): correct react-router-dom import to resolve blank screen

--- pidsus-frontend/src/App.jsx
+++ pidsus-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import DashboardPage from "./pages/DashboardPage";
