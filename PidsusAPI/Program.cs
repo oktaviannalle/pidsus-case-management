74176a7b8d5beb6ccf75e3155e4ac113e35f9c27
oktaviannalle <oktavian.putra@formulatrix.com>
2026-08-12 10:00:20 +0700

    fix: prevent circular reference exception in json serialization

--- PidsusAPI/Program.cs
+++ PidsusAPI/Program.cs
@@ -1,9 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using PidsusAPI.Data;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
